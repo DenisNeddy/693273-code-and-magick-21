64b9196e9bc5c2dc6d5cda0fb7e61ea3ac3099dc
Denis <fordz2012@gmail.com>
2020-09-23 11:58:18 +0300

    m2-t1 Чиска кода

--- js/stat.js
+++ js/stat.js
@@ -1,38 +1,55 @@
-'use strict'
+'use strict';
 const CLOUD_WIDTH = 500;
 const CLOUD_HEIGHT = 280;
+
+const CLOUD_X = 100;
+const GAP = 10;
+const FONT_GAP = 20;
+const FONT_Y = 270;
+const BAR_X = 160;
+const BAR_Y = 100;
+const BAR_WIDTH = 40;
+const BAR_HEIGHT = 150;
+const BAR_GAP = 50;
+const TIMES_Y = 90;
+
 
 const renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
-}
+};
 
-window.renderStatistics = function(ctx) {
-  renderCloud(ctx, 110, 20, 'rgba(0,0,0,0.3)');
-  renderCloud(ctx, 100, 10, '#fff');
+var getMaxElement = function (arr) {
+  var maxElement = arr[0];
+
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+
+  return maxElement;
+};
+
+
+window.renderStatistics = function (ctx, players, times) {
+  renderCloud(ctx, CLOUD_X + GAP, FONT_GAP, 'rgba(0,0,0,0.3)');
+  renderCloud(ctx, CLOUD_X, GAP, '#fff');
 
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', 130, 40);
-  ctx.fillText('Список результатов: ', 130, 65);
+  ctx.fillText('Ура вы победили!', CLOUD_X + GAP + FONT_GAP, BAR_WIDTH);
+  ctx.fillText('Список результатов: ', CLOUD_X + GAP + FONT_GAP, BAR_WIDTH + FONT_GAP);
 
-  ctx.fillStyle = '#000';
-  ctx.fillText('Вы', 160, 270);
-  ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  ctx.fillRect(160, 100, 40, 150);
+  const maxTime = getMaxElement(times);
 
-  ctx.fillStyle = '#000';
-  ctx.fillText('Кекс', 250, 270);
-  ctx.fillStyle = 'hsl(233, 72%, 15%)';
-  ctx.fillRect(250, 100, 40, 150);
+  for (var i = 0; i < players.length; i++) {
+    ctx.fillText(Math.round(times[i]), BAR_X + (BAR_GAP + BAR_WIDTH) * i, TIMES_Y);
 
-  ctx.fillStyle = '#000';
-  ctx.fillText('Егор', 340, 270);
-  ctx.fillStyle = 'hsl(233, 72%, 50%)';
-  ctx.fillRect(340, 100, 40, 150);
+    ctx.fillRect(BAR_X + (BAR_GAP + BAR_WIDTH) * i, BAR_Y, BAR_WIDTH, (BAR_HEIGHT * times[i]) / maxTime);
 
-  ctx.fillStyle = '#000';
-  ctx.fillText('Миша', 430, 270);
-  ctx.fillStyle = 'hsl(233, 72%, 80%)';
-  ctx.fillRect(430, 100, 40, 150);
-}
+    ctx.fillText(players[i], BAR_X + (BAR_GAP + BAR_WIDTH) * i, FONT_Y);
+  }
+
+};
+
